d10a278fd08d524ab832aefeb334df4cbfd9796b
SobhanBera <sobhanbera258@gmail.com>
2021-05-04 10:58:57 +0530

    short installation guide 1. phpMyAdmin 2. apache 3. php 4. mysql-mariadb 5. server this file does not fully covers the procedure of how the softwares could be installed, instead contains a maintained guide to overcome some errors while installing the software.

---
+++ mysql_installation_commands.sh
@@ -0,0 +1,105 @@
+sudo pacman -S phpmyadmin apache php php-apache mariadb gedit
+sudo gedit /etc/php/php.ini
+sudo geany /etc/php/php.ini
+sudo gedit /etc/httpd/conf/httpd.conf
+sudo geany /etc/httpd/conf/httpd.conf
+sudo touch /etc/httpd/conf/extra/phpmyadmin.conf
+sudo geany /etc/httpd/conf/extra/phpmyadmin.conf
+sudo geany /etc/httpd/conf/httpd.conf
+sudo systemctl start httpd
+sudo systemctl start mariadb
+sudo mysql -u root -p 
+sudo mysql -u root -p
+sudo pacman -S mysql
+sudo systemctl start mariadb
+sudo systemctl start mariadb.service
+sudo systemctl start mysql
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+sudo systemctl start mysql
+sudo systemctl start mariadb
+sudo mysql -u root -p 
+sudo systemctl status mariadb
+sudo mysql -u root -p 
+sudo pacman -S phpmyadmin
+sudo pacman -S phpmyadmin apache php
+sudo pacman -S phpmyadmin apache php php-apache
+phpmyadmin
+clear
+sudo pacman -S apache
+sudo geany /etc/httpd/conf/httpd.conf
+sudo systemctl enable httpd
+sudo systemctl restart httpd
+sudo systemctl status httpd
+sudo systemctl start httpd
+sudo systemctl restart httpd
+sudo systemctl status httpd
+sudo geany /etc/httpd/conf/httpd.conf
+sudo geany /etc/httpd/conf/extra/phpmyadmin.conf
+sudo geany /etc/httpd/conf/httpd.conf
+sudo geany /etc/php/php.ini
+sudo systemctl restart httpd
+sudo systemctl status httpd
+sudo systemctl enable httpd
+sudo systemctl restart httpd\
+sudo systemctl status httpd
+sudo pacman -S apache
+sudo systemctl status httpd
+sudo geany /etc/httpd/conf/httpd.conf
+sudo systemctl restart httpd
+sudo systemctl status httpd
+sudo geany /etc/httpd/conf/httpd.conf
+clear
+sudo systemctl restart httpd
+sudo systemctl status httpd
+sudo nano /srv/http/index.html
+sudo systemctl status mysqld
+sudo mysql_secure_installation
+sudo pacman -S php php-apache
+sudo nano /etc/httpd/conf/httpd.conf
+sudo nano /srv/http/test.php
+sudo systemctl restart httpd
+sudo systemctl status mysqld
+sudo systemctl status httpd
+ifconfig
+sudo pacman -S phpmyadmin
+sudo nano /etc/php/php.ini
+sudo nano /etc/httpd/conf/extra/phpmyadmin.conf
+sudo nano /etc/httpd/conf/httpd.conf
+sudo systemctl restart httpd
+sudo systemctl status httpd
+php -v
+sudo nano /etc/webapps/phpmyadmin/config.inc.php
+sudo systemctl restart httpd
+php -m
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+composer dump-autoload
+chmod +x composer.phar
+composer dump-autoload
+./composer.phar -v
+./composer.phar dump-autoload
+touch composer.json
+./composer.phar dump-autoload
+sudo geany composer.json
+./composer.phar dump-autoload
+sudo geany composer.json
+./composer.phar install --no-dev
+./composer.phar install
+./composer.phar update -vvv
+./composer.phar config -g platform-check false
+sudo systemctl restart httpd
+sudo systemctl status httpd
+sudo mv composer.phar /usr/local/bin/composer
+composer require php-http/iconv
+sudo pacman -S php-http
+sudo geany /etc/php/php.ini
+sudo systemctl restart httpd
+sudo systemctl status httpd
+sudo geany /usr/share/webapps/phpMyAdmin/config.inc.php
+sudo systemctl restart httpd
+sudo systemctl status httpd
+geany .zsh_history
+code .zsh_history
